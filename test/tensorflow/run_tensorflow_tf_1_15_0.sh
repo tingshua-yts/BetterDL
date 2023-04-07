1b7097c21ed72aea642d5938162563106b35014c
tingshuai.yts <tingshuai.yts@hotmail.com>
2023-04-07 17:16:20 +0800

    add tf config

---
+++ test/tensorflow/run_tensorflow_tf_1_15_0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+name=tf_cpu_1.15.0
+
+image=tensorflow/tensorflow:1.15.0
+flag=$(sudo docker ps  | grep "$name" | wc -l)
+if [ $flag == 0 ]
+then
+    sudo nvidia-docker stop "$name"
+    sudo nvidia-docker rm "$name"
+    sudo nvidia-docker run --name="$name" -d --net=host \
+         -v /mnt:/mnt \
+    -w /workspace   -it $image /bin/bash
+#    -w /workspace   -it pytorch/pytorch:1.9.0-cuda10.2-cudnn7-devel /bin/bash
+fi
+sudo docker exec -it "$name" bash
